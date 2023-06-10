8a99f13b245547c4143117212f1f8b1f44681855
davatem17 <perezd625@gmail.com>
2023-06-09 21:12:39 -0500

    Datos y Cards

---
+++ src/components/Cards.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <div v-for="item in hotels" class="card">
+            <h3>{{ item.name }}</h3>
+            <p>Contenido de la tarjeta 1</p>
+            <div class="rating">
+                <div v-for="item in item.stars">
+                    <input type="radio" id="star5" name="rating" value="1" checked>
+                    <label for="star5"></label>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import { hotels } from '../data'
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: space-between;
+}
+
+.card {
+    flex-basis: 30%;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 10px;
+    background-color: #f9f9f9;
+}
+
+.rating {
+    display: flex;
+    justify-content: center;
+}
+
+.rating input {
+    display: none;
+}
+
+.rating label {
+    font-size: 30px;
+    color: gold;
+
+}
+
+.rating label:before {
+    content: '\2605';
+}
+</style>
---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const hotels=[
+    {
+      name: "Lakewood",
+      stars: 3,
+      weekRegular: 110,
+      weekendRegular: 90,
+      weekRewards: 80,
+      weekendRewards: 80
+    },{
+      name: "Bridgewood",
+      stars: 4,
+      weekRegular: 160,
+      weekendRegular: 60,
+      weekRewards: 110,
+      weekendRewards: 50 
+    },{
+      name: "Ridgewood",
+      stars: 5,
+      weekRegular: 220,
+      weekendRegular: 150,
+      weekRewards: 100,
+      weekendRewards: 40 
+    }
+  ]
